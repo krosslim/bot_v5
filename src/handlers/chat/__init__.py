0642f9db4f4da7550084b28c4bd9f5735f462ff2
a.telman <aligeydar1111@gmail.com>
2025-10-12 23:50:44 +0300

    fix

--- src/handlers/chat/__init__.py
+++ src/handlers/chat/__init__.py
@@ -2,8 +2,7 @@ from aiogram import Router
 
 from src.handlers.chat.chat_booking_handler import router as chat_booking_router
 from src.handlers.chat.chat_member_handler import router as chat_member_router
-from src.handlers.chat.chat_cancel_handler import router as chat_cancel_router
 
 chat_combined_router = Router()
 
-chat_combined_router.include_routers(chat_booking_router, chat_member_router, chat_cancel_router)
+chat_combined_router.include_routers(chat_booking_router, chat_member_router)
